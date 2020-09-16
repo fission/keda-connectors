02f0ea72b50a2e5a140d0ef4796d0607bc246991
kunal-bagwe <kunal@infracloud.io>
2020-09-16 12:13:16 +0530

    Initial commit for Integration test for Kafka

---
+++ integration/README.md
@@ -0,0 +1,2 @@
+# Keda-Integration
+This repository consists of files relevant to the Integration of Keda connectors
---
+++ integration/consumer/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/Shopify/sarama"
+)
+
+func main() {
+
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+
+	// Specify brokers address. This is default one
+	brokers := []string{"my-cluster-test-kafka-bootstrap:9092"}
+
+	// Create new consumer
+	master, err := sarama.NewConsumer(brokers, config)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := master.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	topic := "response-topic"
+	// How to decide partition, is it fixed value...?
+	consumer, err := master.ConsumePartition(topic, 0, sarama.OffsetOldest)
+	if err != nil {
+		panic(err)
+	}
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+	// Get signnal for finish
+	doneCh := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case err := <-consumer.Errors():
+				fmt.Println(err)
+			case msg := <-consumer.Messages():
+				fmt.Println(string(msg.Key), string(msg.Value))
+			case <-signals:
+				fmt.Println("Interrupt is detected")
+				doneCh <- struct{}{}
+			}
+		}
+	}()
+
+	<-doneCh
+}
