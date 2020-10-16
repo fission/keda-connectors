46e4b2b6533d0125a901679318003ae9d8343fec
GT <gtalekar@infracloud.io>
2020-10-16 12:16:02 +0530

    added kafka pipeline

--- kafka-http-connector/test/consumer/main.go
+++ kafka-http-connector/test/consumer/main.go
@@ -38,7 +38,7 @@ func main() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt)
 
-	// Get signnal for finish
+	// Get signnal for finish test
 	doneCh := make(chan struct{})
 	go func() {
 		for {
