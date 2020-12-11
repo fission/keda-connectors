5bd3e3b410525bd32285b49004d91be7c6a41db7
Richard Noble <ranoble@gmail.com>
2020-12-11 12:32:32 +0000

    Revert adding body to error log. This is a decision outside of this change

--- common/util.go
+++ common/util.go
@@ -2,7 +2,6 @@ package common
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -73,17 +72,10 @@ func HandleHTTPRequest(message string, headers http.Header, data ConnectorMetada
 		// Make the request
 		resp, err = http.DefaultClient.Do(req)
 		if err != nil {
-			body := ""
-			if resp != nil {
-				defer resp.Body.Close()
-				b, _ := ioutil.ReadAll(resp.Body)
-				body = string(b)
-			}
 			logger.Error("sending function invocation request failed",
 				zap.Error(err),
 				zap.String("http_endpoint", data.HTTPEndpoint),
-				zap.String("source", data.SourceName),
-				zap.String("response", body))
+				zap.String("source", data.SourceName))
 			continue
 		}
 		if resp == nil {
