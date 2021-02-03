c189c31fe8e3db5fab5cfc87b9283d7d1e346640
Jacob <chenjr0719@gmail.com>
2021-02-03 17:06:07 +0800

    fix: Fix if condition when request failure (#48)

--- common/util.go
+++ common/util.go
@@ -91,7 +91,7 @@ func HandleHTTPRequest(message string, headers http.Header, data ConnectorMetada
 		return nil, fmt.Errorf("every function invocation retry failed; final retry gave empty response. http_endpoint: %v, source: %v", data.HTTPEndpoint, data.SourceName)
 	}
 
-	if resp.StatusCode < 200 && resp.StatusCode > 300 {
+	if resp.StatusCode < 200 || resp.StatusCode > 300 {
 		return nil, fmt.Errorf("request returned failure: %v. http_endpoint: %v, source: %v", resp.StatusCode, data.HTTPEndpoint, data.SourceName)
 	}
 	return resp, nil
