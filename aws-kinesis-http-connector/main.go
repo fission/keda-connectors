72ffb46087e6b1c479c8cb1f3d3b72e8f343943e
Sanket Sudake <sanketsudake@gmail.com>
2021-07-09 11:09:53 +0530

    Add sync maps and error in aws kinesis connector (#66)
    * Add sync maps and error in aws kinesis connector
    * Update Go version 1.15
    * Update kinesis connector version to 0.7

--- aws-kinesis-http-connector/main.go
+++ aws-kinesis-http-connector/main.go
@@ -47,7 +47,7 @@ func (conn *awsKinesisConnector) listShards() ([]*kinesis.Shard, error) {
 
 //findNewShards sends shards, it only sends newly added shards
 func (conn *awsKinesisConnector) findNewShards() {
-	shards := make(map[string]*kinesis.Shard)
+	var shards sync.Map
 	var ticker = time.NewTicker(30 * time.Second)
 	for {
 		select {
@@ -63,15 +63,13 @@ func (conn *awsKinesisConnector) findNewShards() {
 
 			for _, s := range shardList {
 				//send only new shards
-				if _, ok := shards[*s.ShardId]; ok {
-					continue
+				_, loaded := shards.LoadOrStore(*s.ShardId, s)
+				if !loaded {
+					conn.shardc <- s
 				}
-				shards[*s.ShardId] = s
-				conn.shardc <- s
 			}
 		}
 	}
-
 }
 
 //getIterator get's the iterator either from start or from where we left
@@ -85,7 +83,7 @@ func (conn *awsKinesisConnector) getIterator(shardID string, checkpoint string) 
 		//Start from, where we left
 		params.StartingSequenceNumber = aws.String(checkpoint)
 		params.ShardIteratorType = aws.String(kinesis.ShardIteratorTypeAfterSequenceNumber)
-		iteratorOutput, err := conn.client.GetShardIteratorWithContext(aws.Context(conn.ctx), params)
+		iteratorOutput, err := conn.client.GetShardIteratorWithContext(conn.ctx, params)
 		if err != nil {
 			return nil, err
 		}
@@ -93,7 +91,7 @@ func (conn *awsKinesisConnector) getIterator(shardID string, checkpoint string) 
 	}
 	//Start from, oldest record in the shard
 	params.ShardIteratorType = aws.String(kinesis.ShardIteratorTypeTrimHorizon)
-	iteratorOutput, err := conn.client.GetShardIteratorWithContext(aws.Context(conn.ctx), params)
+	iteratorOutput, err := conn.client.GetShardIteratorWithContext(conn.ctx, params)
 	if err != nil {
 		return nil, err
 	}
@@ -122,12 +120,12 @@ func isShardClosed(nextShardIterator, currentShardIterator *string) bool {
 //scan each shards for any new records, when found call the passed func
 func (conn *awsKinesisConnector) pullRecords(fn pullFunc) {
 	//checkpoints to identify how much read has happened
-	checkpoints := make(map[string]string)
+	var checkpoints sync.Map
 	var wg sync.WaitGroup
 	//get called when any new shards are added
 	for s := range conn.shardc {
 		//Start fresh
-		checkpoints[*s.ShardId] = ""
+		checkpoints.Store(*s.ShardId, "")
 		wg.Add(1)
 		go func(shardID string) {
 			defer wg.Done()
@@ -136,10 +134,12 @@ func (conn *awsKinesisConnector) pullRecords(fn pullFunc) {
 			defer scanTicker.Stop()
 			for {
 				//do noting if shard got deleted
-				if _, found := checkpoints[shardID]; !found {
+				checkpoint, found := checkpoints.Load(shardID)
+				if !found {
+					conn.logger.Info("shard not found", zap.String("shardID", shardID))
 					return
 				}
-				iteratorOutput, err := conn.getIterator(shardID, checkpoints[shardID])
+				iteratorOutput, err := conn.getIterator(shardID, checkpoint.(string))
 				if err != nil {
 					conn.logger.Error("error in iterator",
 						zap.String("shardID", shardID),
@@ -159,7 +159,7 @@ func (conn *awsKinesisConnector) pullRecords(fn pullFunc) {
 					for _, r := range resp.Records {
 						//send records
 						err := fn(&record{r, shardID, resp.MillisBehindLatest})
-						checkpoints[shardID] = *r.SequenceNumber
+						checkpoints.Store(shardID, *r.SequenceNumber)
 						if err != nil {
 							conn.logger.Error("error in processing records",
 								zap.String("shardID", shardID),
@@ -168,8 +168,8 @@ func (conn *awsKinesisConnector) pullRecords(fn pullFunc) {
 					}
 					if isShardClosed(resp.NextShardIterator, iterator) {
 						//when shards got deleted, remove it from checkpoints
-						if _, found := checkpoints[shardID]; found {
-							delete(checkpoints, shardID)
+						if _, found := checkpoints.Load(shardID); found {
+							checkpoints.Delete(shardID)
 							return
 						}
 					}
@@ -198,23 +198,34 @@ func (conn *awsKinesisConnector) consumeMessage(r *record) {
 
 	resp, err := common.HandleHTTPRequest(string(r.Data), headers, conn.connectordata, conn.logger)
 	if err != nil {
+		conn.logger.Error("error processing message",
+			zap.String("shardID", r.shardID),
+			zap.Error(err))
 		conn.errorHandler(r, err.Error())
 	} else {
 		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
+			conn.logger.Error("error processing message",
+				zap.String("shardID", r.shardID),
+				zap.Error(err))
 			conn.errorHandler(r, err.Error())
 		} else {
-			if success := conn.responseHandler(r, string(body)); success {
-				conn.logger.Info("done processing message",
-					zap.String("shardID", r.shardID),
-					zap.String("message", string(body)))
+			if err := conn.responseHandler(r, string(body)); err != nil {
+				conn.logger.Error("failed to publish response body from http request to topic",
+					zap.Error(err),
+					zap.String("topic", conn.connectordata.ResponseTopic),
+					zap.String("source", conn.connectordata.SourceName),
+					zap.String("http endpoint", conn.connectordata.HTTPEndpoint))
 			}
+			conn.logger.Info("done processing message",
+				zap.String("shardID", r.shardID),
+				zap.String("message", string(body)))
 		}
 	}
 }
 
-func (conn *awsKinesisConnector) responseHandler(r *record, response string) bool {
+func (conn *awsKinesisConnector) responseHandler(r *record, response string) error {
 	if len(conn.connectordata.ResponseTopic) > 0 {
 		params := &kinesis.PutRecordInput{
 			Data:                      []byte(response),                             // Required
@@ -222,19 +233,12 @@ func (conn *awsKinesisConnector) responseHandler(r *record, response string) boo
 			StreamName:                aws.String(conn.connectordata.ResponseTopic), // Required
 			SequenceNumberForOrdering: aws.String(*r.SequenceNumber),
 		}
-
 		_, err := conn.client.PutRecord(params)
 		if err != nil {
-			conn.logger.Error("failed to publish response body from http request to topic",
-				zap.Error(err),
-				zap.String("topic", conn.connectordata.ResponseTopic),
-				zap.String("source", conn.connectordata.SourceName),
-				zap.String("http endpoint", conn.connectordata.HTTPEndpoint),
-			)
-			return false
+			return err
 		}
 	}
-	return true
+	return nil
 }
 
 func (conn *awsKinesisConnector) errorHandler(r *record, errMsg string) {
@@ -280,19 +284,18 @@ func main() {
 	}
 
 	s, err := session.NewSession(config)
-	kc := kinesis.New(s)
-
 	if err != nil {
-		logger.Error("not able to create the session")
+		logger.Error("not able to create the session", zap.Error(err))
 		return
 	}
+	kc := kinesis.New(s)
 	connectordata, err := common.ParseConnectorMetadata()
 	if err != nil {
-		logger.Error("error while parsing metadata")
+		logger.Error("error while parsing metadata", zap.Error(err))
 		return
 	}
 	if err := kc.WaitUntilStreamExists(&kinesis.DescribeStreamInput{StreamName: &connectordata.Topic}); err != nil {
-		logger.Error("not able to connect to kinesis stream")
+		logger.Error("not able to connect to kinesis stream", zap.Error(err))
 		return
 	}
 
@@ -313,7 +316,7 @@ func main() {
 		shardc:        shardc,
 		maxRecords:    10, //Read maximum 10 records
 	}
-
+	logger.Info("Starting aws kinesis connector")
 	//Get the shards in shardc chan
 	go func() {
 		conn.findNewShards()
