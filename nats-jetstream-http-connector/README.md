5e6e7475ba3f6c3b30068e44709e6af15cdf0927
neha_gupta <nehagupta161995@gmail.com>
2022-08-23 12:40:37 +0530

    Jetstream connector (#109)
    * keda jetstream connector
    * update README
    * update ioutil package
    * add jetstream to release.yml
    * testcase: keep pod running forever
    * update import
    * add method for testing
    * update fission consumer name
    * update README file
    
    Signed-off-by: Sanket Sudake <sanketsudake@gmail.com>
    Co-authored-by: Sanket Sudake <sanketsudake@gmail.com>

---
+++ nats-jetstream-http-connector/README.md
@@ -0,0 +1,27 @@
+# JetStream NATS KEDA Connector
+
+JetStream NATS KEDA connector image can be used in the Kubernetes deployment as scaleTargetRef in scaledObject of [NATs scaler](https://keda.sh/docs/2.8/scalers/nats-jetstream/).
+
+The job of the connector is to read messages from the subject in the given stream, call an HTTP endpoint with the body of the message, and write response or error in the response_topic. Following enviornment variables are used by connector image as configuration to connect and authenticate with NATs server which should be defined in the Kubernetes deployment manifest.
+
+- `TOPIC`: Subject from which messages are read. It is generally of form - `streamname.subjectname`
+- `HTTP_ENDPOINT`: http endpoint to post request
+- `RESPONSE_TOPIC`: Subject to write responses on success response.  It is generally of form - `response_stream_name.response_subject_name` where streamname should be different then input stream. `response_stream_name` is output stream name. `response_subject_name` subject name where output is send.
+- `ERROR_TOPIC`: Subject to write errors on failure.  It is generally of form - `err_response_stream_name.error_subject_name` where streamname should be different then input stream. `err_response_stream_name` is error stream name. `error_subject_name` subject name where error output is send.
+- `MAX_RETRIES`: Maximum number of times an http endpoint will be retried upon failure.
+- `CONTENT_TYPE`: Content type used while creating post request
+- `NATS_SERVER`: NATS server address. It can be a remote address `nats://127.0.0.1:4222` or in case deployed in Kubernetes, can reached using corresponding service name
+- `STREAM`: stream from which connector will read messages.
+- `FISSION_CONSUMER`: this is the consumer which fission uses to pull data from stream, proccesses it and pushes it forward to `RESPONSE_TOPIC`
+
+
+
+## Resources
+* To setup and run nats streaming server, reference https://docs.nats.io/nats-server/installation#installing-on-kubernetes-with-nats-operator
+* For running the connecter with fission e.g.  
+
+``` fission mqt create --name jetstreamtest1 --function helloworld --mqtype nats-jetstream --mqtkind keda --topic input.created --resptopic output.response-topic --errortopic output.error-topic --maxretries 3 --metadata stream=input --metadata fissionConsumer= fission_consumer --metadata natsServerMonitoringEndpoint=nats-jetstream.default.svc.cluster.local:8222  --metadata natsServer=nats://nats-jetstream.default.svc.cluster.local:4222 ```
+
+ 
+          
+
---
+++ nats-jetstream-http-connector/main.go
@@ -0,0 +1,224 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+
+	"github.com/nats-io/nats.go"
+	"go.uber.org/zap"
+
+	"github.com/fission/keda-connectors/common"
+)
+
+var (
+	FISSION_CONSUMER = "fission_consumer" // TODO: need to change this
+)
+
+type jetstreamConnector struct {
+	host          string
+	connectordata common.ConnectorMetadata
+	jsContext     nats.JetStreamContext
+	logger        *zap.Logger
+}
+
+func (conn jetstreamConnector) consumeMessage() {
+
+	sub, err := conn.jsContext.PullSubscribe(os.Getenv("TOPIC"), FISSION_CONSUMER, nats.PullMaxWaiting(512))
+	if err != nil {
+		conn.logger.Fatal("error occurred while consuming message", zap.Error(err))
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	for {
+		select {
+		case <-signalChan:
+			ctx.Done()
+			conn.logger.Info("Received an interrupt, unsubscribing and closing connection...")
+			err = sub.Unsubscribe()
+			if err != nil {
+				conn.logger.Error("error occurred while unsubscribing", zap.Error(err))
+			}
+			err = conn.jsContext.DeleteConsumer(os.Getenv("STREAM"), FISSION_CONSUMER)
+			if err != nil {
+				conn.logger.Error("error occurred while closing connection", zap.Error(err))
+			}
+			return
+		default:
+		}
+		msgs, _ := sub.Fetch(10, nats.Context(ctx))
+		for _, msg := range msgs {
+			conn.handleHTTPRequest(msg)
+
+		}
+	}
+
+}
+
+func (conn jetstreamConnector) handleHTTPRequest(msg *nats.Msg) {
+
+	headers := http.Header{
+		"Topic":        {conn.connectordata.Topic},
+		"RespTopic":    {conn.connectordata.ResponseTopic},
+		"ErrorTopic":   {conn.connectordata.ErrorTopic},
+		"Content-Type": {conn.connectordata.ContentType},
+		"Source-Name":  {conn.connectordata.SourceName},
+	}
+	resp, err := common.HandleHTTPRequest(string(msg.Data), headers, conn.connectordata, conn.logger)
+	if err != nil {
+		conn.logger.Info(err.Error())
+		conn.errorHandler(err)
+		// mqtpod
+	} else {
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			conn.logger.Info(err.Error())
+			conn.errorHandler(err)
+		} else {
+			if success := conn.responseHandler(body); success {
+				err = msg.Ack()
+				if err != nil {
+					conn.logger.Info(err.Error())
+					conn.errorHandler(err)
+				}
+				conn.logger.Info("Done processing message",
+					zap.String("messsage", string(body)))
+			}
+		}
+	}
+
+}
+
+func (conn jetstreamConnector) errorHandler(err error) {
+
+	if len(conn.connectordata.ErrorTopic) == 0 {
+		conn.logger.Warn("Error topic not set")
+		return
+	}
+
+	_, publishErr := conn.jsContext.Publish(conn.connectordata.ErrorTopic, []byte(err.Error()))
+
+	if publishErr != nil {
+		conn.logger.Error("failed to publish message to error topic",
+			zap.Error(publishErr),
+			zap.String("source", conn.connectordata.SourceName),
+			zap.String("message", publishErr.Error()),
+			zap.String("topic", conn.connectordata.ErrorTopic))
+	}
+}
+
+func (conn jetstreamConnector) responseHandler(response []byte) bool {
+	if len(conn.connectordata.ResponseTopic) == 0 {
+		conn.logger.Warn("Response topic not set")
+		return false
+	}
+
+	// we consider the subject is of form - stream.subjectName.
+	// We split it and use the first word to create and stream if not found.
+	// Push the responses to the above created stream.
+	responseTopic := strings.Split(conn.connectordata.ResponseTopic, ".")
+	err := conn.createStream(conn.jsContext, responseTopic[0], conn.connectordata.ResponseTopic)
+	if err != nil {
+		conn.logger.Error("failed to publish response body from http request to topic",
+			zap.Error(err),
+			zap.String("topic", conn.connectordata.ResponseTopic),
+			zap.String("source", conn.connectordata.SourceName),
+			zap.String("http endpoint", conn.connectordata.HTTPEndpoint),
+		)
+		return false
+	}
+	_, publishErr := conn.jsContext.Publish(conn.connectordata.ResponseTopic, response)
+	if publishErr != nil {
+		conn.logger.Error("failed to publish response body from http request to topic",
+			zap.Error(publishErr),
+			zap.String("topic", conn.connectordata.ResponseTopic),
+			zap.String("source", conn.connectordata.SourceName),
+			zap.String("http endpoint", conn.connectordata.HTTPEndpoint),
+		)
+		return false
+	}
+	return true
+}
+
+// createStream creates a stream by using JetStreamContext
+func (conn jetstreamConnector) createStream(js nats.JetStreamContext, streamName string, streamSubjects string) error {
+	stream, err := js.StreamInfo(streamName)
+	if err != nil {
+		conn.logger.Info("stream not present will create one",
+			zap.String("name", streamName))
+	}
+	if stream == nil {
+		conn.logger.Info("creating stream",
+			zap.String("name", streamName),
+			zap.String("and subjects ", streamSubjects))
+		_, err = js.AddStream(&nats.StreamConfig{
+			Name:     streamName,
+			Subjects: []string{streamSubjects},
+		})
+		if err != nil {
+			conn.logger.Error("failed to publish response body from http request to topic",
+				zap.Error(err))
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("can't initialize zap logger: %v", err)
+	}
+
+	var nc *nats.Conn
+	defer func() {
+		logger.Sync()
+		nc.Close()
+	}()
+
+	connectordata, err := common.ParseConnectorMetadata()
+	if err != nil {
+		logger.Fatal("error occurred while parsing metadata", zap.Error(err))
+	}
+	host := os.Getenv("NATS_SERVER")
+
+	if host == "" {
+		logger.Fatal("received empty host field")
+	}
+
+	// Connect to NATS
+	nc, err = nats.Connect(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	js, err := nc.JetStream() // TODO: need to configure this
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = js.AddConsumer(os.Getenv("STREAM"), &nats.ConsumerConfig{
+		Durable:   FISSION_CONSUMER,
+		AckPolicy: nats.AckExplicitPolicy,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn := jetstreamConnector{
+		host:          host,
+		jsContext:     js,
+		connectordata: connectordata,
+		logger:        logger,
+	}
+
+	conn.consumeMessage()
+}
